f5eb5656af87688fe616eb0ee140773452e8121f
Gustavo Duarte <gus.duarte@gmail.com>
2015-02-19 11:39:20 -0200

    Creo un modulo message comun a gtk2 y gtk3 y algunos fixes

--- webkit/Notificador-instalador/notifier/viewer_gtk3.py
+++ webkit/Notificador-instalador/notifier/viewer_gtk3.py
@@ -12,116 +12,8 @@ from ceibal.notifier import env
 from ceibal.notifier.data_base import Db
 from ceibal.notifier.utilidades import *
 from ceibal.notifier.constantes import * 
+from ceibal.notifier.message  import * 
 from os.path import expanduser
-
-
-class Messages:
-    db = None
-
-    def __init__(self):
-        db_filename = os.path.join(env.get_data_root(),DB_FILE)        
-        self.file_name = os.path.join(expanduser("~"),READ_FILE)    
-       
-        if Messages.db is None:
-            Messages.db = Db(db_filename)
-
-
-    def _save_notification_read(self, id):
-        try:
-            # Cargo el diccionario desde el archivo json
-            fp = open(self.file_name, "r")
-            notif_read_record = json.load(fp)
-            fp.close()
-            
-            # Borro el archivo json
-            open(self.file_name,'w').close()
-            fp = open(self.file_name,'r+')
-        except:
-            # El archivo json no existe, lo creo.
-            fp = open(self.file_name, "w+")
-            notif_read_record = {}
-        
-        notif_read_record[id] = 'read'
-        json.dump(notif_read_record, fp)
-        fp.flush()
-        fp.close()
-        
-
-    def _check_notification_is_unread (self, message):
-        id = str (message['id'])
-        
-        try:
-            fp = open(self.file_name, "r") 
-            notif_read_record = json.load(fp)
-        except:
-            print "No existe el archivo de registro ...."
-            return True
-          
-        if fp is not None:
-          fp.close()
-
-        if id in notif_read_record:
-            return notif_read_record[id] == 'unread'
-        else:
-            print "No encuentro id en el registro ..." + id
-            return True
-
-    def _date_valid(self,message):
-        '''
-        @param message: Mensaje a validar.
-        
-        @summary: Valida si el mensaje ya expiro.
-        
-        @return: Verdadero si esta vigente el mensaje.
-        '''
-        expires = message["vencimiento"]
-        today = datetime.datetime.strftime(datetime.date.today(), "%Y-%m-%d")
-        return today <= expires
-
-
-    def get_first_unread(self, args={}):
-        messages = filter(self._date_valid,Messages.db.get_messages(args))
-        messages_unread = filter(self._check_notification_is_unread, messages)
-        if len(messages_unread) > 0:
-            return messages_unread[0]
-        else:
-            return None 
-    
-    def get_next_unread(self, message):
-        messages = filter(self._date_valid,Messages.db.get_messages({}))
-        messages_unread = filter(self._check_notification_is_unread, messages)
-        
-        for msg in messages_unread:
-            if msg['id'] > message['id']:
-                return msg
-        else:
-            return None 
-    
-    def get_prev_unread(self, message):
-        messages = filter(self._date_valid,Messages.db.get_messages({}))
-        messages_unread = filter(self._check_notification_is_unread, messages)
-
-        msg_prev = None 
-        for msg in messages_unread:
-            if msg['id'] < message['id']:
-                msg_prev = msg
-            else:
-                return msg_prev
- 
-        return msg_prev 
-       
-   
-    def get_all(self):
-        return Messages.db.get_messages({})
- 
-    def get_test_message(self):
-        wdir = os.path.dirname(os.path.abspath(__file__))
-        return ('file://' + os.path.join(wdir, 'message.html'))
-    
-    def set_read(self, message):
-        self._save_notification_read(message['id'])
-
-
 
 
 
@@ -137,6 +29,7 @@ class VentanaBoton(Gtk.Window):
         self.set_skip_taskbar_hint(True)
         
         if self.message_mgr.get_first_unread() is None:
+            print "No hay mensajes o estan todos leidos ..."
             return
     
         self.set_decorated(False)
@@ -180,7 +73,7 @@ class Visor(Gtk.Window):
         self.set_decorated(False)
 
         self.resize(400,300)
-        self.move(Gdk.Screen.get_default() - self.get_size()[0] ,0)
+        self.move(Gdk.Screen.get_default().get_width() - self.get_size()[0] ,0)
 
         self.set_accept_focus(False)
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
