edab5748f4a788748157ba0484ae9ad52864088d
Gustavo Duarte <gus.duarte@gmail.com>
2015-03-09 20:35:08 -0300

    Creo un viewer common a gtk2/gtk3, gtk3

--- webkit/Notificador-instalador/notifier/viewer_gtk3.py
+++ webkit/Notificador-instalador/notifier/viewer_gtk3.py
@@ -7,7 +7,7 @@ import os
 import urlparse
 
 from ceibal.notifier import env
-from ceibal.notifier.data_base import Db
+from ceibal.notifier.viewer import *
 from ceibal.notifier.utilidades import *
 from ceibal.notifier.constantes import * 
 from ceibal.notifier.message  import * 
@@ -79,11 +79,11 @@ class Visor(Gtk.Window):
 
 
 
-class WebViewer:
+class WebViewer (WebViewerCommon):
 
     def __init__ (self,win):
+        WebViewerCommon.__init__(self)
         self.win = win
-        self.mode = 'unread'
         self.view = WebKit.WebView()
         self.view.connect("navigation-policy-decision-requested",self.navigate)
         self.sw = Gtk.ScrolledWindow()
@@ -94,79 +94,21 @@ class WebViewer:
         
         self.win.box.pack_start(self.sw, True, True, 0)
         self.win.box.pack_start(self.btn_leido, True, True, 0)
-        self.current_msg = None
         
     def create_btn_leido(self):
         button = Gtk.ToggleButton(label='Marcar como leída')
         button.connect("toggled", self.btn_leido_cb, "Boton leido presionado")
         return button
 
-    def btn_leido_cb(self, widget, data=None):
-        if widget.get_active():
-            self.win.message_mgr.set_read(self.current_msg)
-            widget.set_label('Marcar como no leída')
-        else:
-            self.win.message_mgr.set_unread(self.current_msg)
-            widget.set_label ('Marcar como leída')
-    
-    def update_read_button(self, msg):
-        if msg is None:
-            self.btn_leido.set_sensitive(False)
-        else:
-            self.btn_leido.set_sensitive(True)
-            if self.win.message_mgr.is_unread(msg):
-                self.btn_leido.set_active(False)        
-            else:     
-                self.btn_leido.set_active(True)        
-     
-    def show_msg (self, pos):
-        if self.current_msg is None:
-            self.current_msg = self.win.message_mgr.get_first()
-
-        
-        if pos == 'next':
-            current_msg = self.win.message_mgr.get_next_unread(self.current_msg) if (self.mode == 'unread') else self.win.message_mgr.get_next(self.current_msg)
-        elif pos == 'prev':
-            current_msg = self.win.message_mgr.get_prev_unread(self.current_msg) if (self.mode == 'unread') else self.win.message_mgr.get_prev(self.current_msg)
-        elif pos == 'first':
-            current_msg = self.win.message_mgr.get_first_unread() if (self.mode == 'unread') else self.win.message_mgr.get_first()
-        else:
-            current_msg = None
-        
-        if current_msg is not None:
-            self.current_msg = current_msg
-            self.view.load_string(current_msg['html'], 'text/html', 'UTF-8','/')   
-            self.win.tool_bar.update_msg_counter(str(self.win.message_mgr.get_pos(self.mode, current_msg)), 
-                                                            str(self.win.message_mgr.get_total(self.mode))) 
-        self.win.tool_bar.update_next_back_buttons(current_msg)
-        self.update_read_button(current_msg)
-
-    
-    def set_msg_read(self):
-        self.win.message_mgr.set_read(self.current_msg)
-
-    def set_mode(self, mode):
-        self.mode = mode
-
-    def navigate(self, view, frame, request, action, decision):
-        uri = request.get_uri()
-        parts = urlparse.urlsplit(uri)
-        if parts.scheme and parts.netloc: 
-            open_external_browser(uri)
-            return True
-
-        return False
 
 
-
-
-class ToolBar(Gtk.Toolbar):
+class ToolBar(ToolBarCommon):
     
     def __init__(self, win):
-        Gtk.Toolbar.__init__(self)
-        self.set_style(Gtk.ToolbarStyle.ICONS)
+        self.tbar = Gtk.Toolbar()
+        self.tbar.set_style(Gtk.ToolbarStyle.ICONS)
         self.win = win
-        self.win.box.pack_start(self, False, False, 0)
+        self.win.box.pack_start(self.tbar, False, False, 0)
         
         self.back = Gtk.ToolButton(Gtk.STOCK_GO_BACK, label="Anterior")
         self.back.connect("clicked", self.on_back_clicked)
@@ -192,57 +134,16 @@ class ToolBar(Gtk.Toolbar):
         self.msg_counter = Gtk.Label() 
         msg_counter_item.add (self.msg_counter)
 
-        self.insert(self.back, 0)
-        self.insert(msg_counter_item,1)
-        self.insert(self.next, 2)
-        self.insert(sep, 3)
-        self.insert(check_item,4)
-        self.insert(self.close, 5)
+        self.tbar.insert(self.back, 0)
+        self.tbar.insert(msg_counter_item,1)
+        self.tbar.insert(self.next, 2)
+        self.tbar.insert(sep, 3)
+        self.tbar.insert(check_item,4)
+        self.tbar.insert(self.close, 5)
 
-    def update_msg_counter(self, id, total):
-        self.msg_counter.set_text(id + '/' + total)
-
-    def toggled (self, obj):
-        if obj.get_active ():
-            self.win.html_viewer.set_mode('all')
-        else:
-            self.win.html_viewer.set_mode('unread')
-        self.win.html_viewer.show_msg('first')
- 
-    def on_next_clicked(self, widget):
-        print("Siguiente")
-        self.win.html_viewer.show_msg('next')
-
-    def on_back_clicked(self, widget):
-        print("Atras")            
-        self.win.html_viewer.show_msg('prev')
-    
     def on_close_clicked(self, widget):
         print("Goodbye")            
         self.win.destroy() 
         if self.win.message_mgr.get_first_unread() is None:
             Gtk.main_quit()
-
-    def update_next_back_buttons(self, msg):
-        
-        if self.win.html_viewer.mode == 'all':
-            if self.win.message_mgr.get_prev(msg) is None:
-                self.back.set_sensitive(False)
-            else:
-                self.back.set_sensitive(True)
-            
-            if self.win.message_mgr.get_next(msg) is None:
-                self.next.set_sensitive(False)
-            else:
-                self.next.set_sensitive(True)
-        else:
-            if self.win.message_mgr.get_prev_unread(msg) is None:
-                self.back.set_sensitive(False)
-            else:
-                self.back.set_sensitive(True)
-            
-            if self.win.message_mgr.get_next_unread(msg) is None:
-                self.next.set_sensitive(False)
-            else:
-                self.next.set_sensitive(True)
 
