2b96f8498838690826f17035607f28766bf5763d
Gustavo Duarte <gus.duarte@gmail.com>
2015-05-09 11:16:06 -0300

    Sugar Activity Visor GTK2 / GTK3

--- webkit/Notificador-instalador/sugar/NotificadorVisor.activity/activity.py
+++ webkit/Notificador-instalador/sugar/NotificadorVisor.activity/activity.py
@@ -26,6 +26,7 @@ else:
     from ceibal.notifier.viewer_gtk3 import *
     from sugar3.activity import activity
 
+winGtk = WinGtk()
 
 class NotificadorVisor(activity.Activity):
 
@@ -33,22 +34,21 @@ class NotificadorVisor(activity.Activity):
         _logger.debug('Start NotificadorActivity')
         activity.Activity.__init__(self,handle)
 
-        (self.width, self.height) = get_window_size()
+        self.width  = winGtk.get_screen_with()
+        self.height = winGtk.get_screen_height()
+        self.web_viewer_height = (self.height - 120)
+
         self.connect("key-release-event", self.on_key_release)
-        # Evita que aparezca en la lista de ventanas
-        self.set_skip_taskbar_hint(True)
 
-        # Decorators
-        self.set_decorated(False)
-
-        self.resize(self.width, self.height)
+        #self.resize(self.width, self.height)
+        #(pos_h, pos_v) = get_window_pos(winGtk.get_screen_with() - self.get_size()[0])
+        #self.move(pos_h, pos_v)
         self.set_accept_focus(False)
-        self.box = gtk.VBox()
+        self.box = winGtk.get_VBox()
         self.message_mgr = Messages()
         self.tool_bar = ToolBar(self)
         self.html_viewer = WebViewer(self)
-        (pos_h, pos_v) = get_window_pos(gtk.gdk.screen_width() - self.get_size()[0])
-        self.move(pos_h, pos_v)
+
         self.html_viewer.show_msg('first')
         self.set_canvas(self.box)
         self.box.show_all()
@@ -56,7 +56,7 @@ class NotificadorVisor(activity.Activity):
 
     def custom_close(self):
         self.destroy()
-        gtk.mainquit()
+        winGtk.quit()
 
     def on_key_release(self, widget, ev, data=None):
         print "Key pressed"
