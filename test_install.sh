62f144f0a3e5618d5cb70ee222d1837116e45ee1
Gustavo Duarte <gus.duarte@gmail.com>
2015-08-28 11:54:42 -0300

    Reorganizo las carpetas

--- test_install.sh
+++ test_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-SERVER=ceibal@192.168.1.122
 SERVER=xo15
 SERVER=xo175
-SERVER=ub14-32
+SERVER=test12
+SERVER=ceibal@192.168.0.106
 
 ssh $SERVER <<'CLEAN'
 cd devel/tmp
 rm -rf webkit
 CLEAN
 
-scp -r webkit $SERVER:./devel/tmp/
+scp -r instalador $SERVER:./devel/tmp/
 ssh $SERVER <<'INSTALL' 
-cd devel/tmp/webkit/Notificador-instalador
+cd devel/tmp/instalador
 echo 'gustavo' | sudo -S bash instalar.sh
 INSTALL
